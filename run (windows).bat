6c3d682f95ba5633538090a8a47e25cf05e0bd37
CubeConflict <vod-k@live.be>
2024-09-19 20:43:55 +0200

    misc | running with checkpoint model by default

--- run (windows).bat
+++ run (windows).bat
@@ -8,7 +8,7 @@ REM Set PYTHONPATH to the root directory to include the loraw subfolder
 set PYTHONPATH=%CD%;%CD%\loraw
 
 REM Run the Python script as if it is in the root folder
-python loraw\run_gradio.py --ckpt-path ".\models\checkpoints\model.safetensors" --model-config ".\models\checkpoints\model_config.json" --lora-dir ".\models\loras"
+python loraw\run_gradio.py --ckpt-path ".\models\checkpoints\model.ckpt" --model-config ".\models\checkpoints\model_config.json" --lora-dir ".\models\loras"
 
 REM Pause to keep the terminal open
 pause
